3cacdbfc67f0eb8c80b3462c91ea356021f2f746
Ricardo Pereira <patresio@gmail.com>
2023-02-14 16:03:30 -0300

    Ajuste search_input

--- base/views.py
+++ base/views.py
@@ -55,7 +55,7 @@ class TaskList(LoginRequiredMixin, ListView):
         search_input = self.request.GET.get('search-area') or ''
         
         if search_input:
-            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+            context['tasks'] = context['tasks'].filter(title__contains=search_input)
 
         context['search_input'] = search_input
 
